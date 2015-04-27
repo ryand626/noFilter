119baee536bbc3913c5823239ac999b34b6411fb
Ryan Dougherty <ryand626@gmail.com>
2015-04-26 20:05:32 -0400

    Initial Commit

---
+++ noFilter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  noFilter
+//
+//  Created by Ryan Dougherty on 4/26/15.
+//  Copyright (c) 2015 team17. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ noFilter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  noFilter
+//
+//  Created by Ryan Dougherty on 4/26/15.
+//  Copyright (c) 2015 team17. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
